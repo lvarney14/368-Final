2de798ad1e9816dad5175a79fc242db4eaa36bef
Lauren Varney <l_varney_14@hotmail.com>
2013-12-10 17:31:02 -0800

    Final 12/10

---
+++ insert.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact</title>
+
+</head>
+<body>
+
+$name=$_POST['name'];
+$city=$_POST['city'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+ 
+$to = "l_varney_14@hotmail.com";
+$subject = "Future Tutorials Contact Form Test";
+$message = " Name: " . $name . "\r\n City: " . $city . "\r\n Phone: " . $phone . "\r\n Email: " . $email;
+ 
+ 
+$from = "FutureTutorials";
+$headers = "From:" . $from . "\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+ 
+if(@mail($to,$subject,$message,$headers))
+{
+  print "<script>document.location.href='http://demo.ftutorials.com/html5-contact-form/success.html';</script>";
+  // Created by Future Tutorials
+}else{
+  echo "Error! Please try again.";
+}
+</body>
+</html>
